7d6ab3c8016bcd181b3457bf1dd44e07cc053033
ThongD <thong.do@gumiviet.com>
2020-05-11 15:02:17 +0700

    git set

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sit non consectetur voluptate repellendus ratione laudantium dolores. Debitis sapiente, dignissimos non quo neque iste nobis inventore a voluptas nesciunt illo?</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sit non consectetur voluptate repellendus ratione laudantium dolores. Debitis sapiente, dignissimos non quo neque iste nobis inventore a voluptas nesciunt illo?</p>
+        </div>
+    );
+}
+
+export default Contact;
