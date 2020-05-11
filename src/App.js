6fdb9f8f249db43c7b1f9ca6c643eb1f0ff53381
ThongD <thong.do@gumiviet.com>
2020-05-11 15:25:41 +0700

    Link NavLink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route path='/' exact component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Contact' component={Contact} />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navber = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a href="" className='brand-logo'>Poke</a>
+                <Link to='/' className='brand-logo'>Poke</Link>
                 <ul className='right'>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
         </nav>
